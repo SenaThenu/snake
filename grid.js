f81c5c911470c48877dedc477400b4940471ddda
Senash Thenuja <senathenu@gmail.com>
2024-01-07 01:06:59 +1300

    limited the food's spawning to be within the grid

--- grid.js
+++ grid.js
@@ -5,8 +5,8 @@ let GRID_SIZE = 21;
 
 export function randomGridPosition() {
     return {
-        x: Math.floor(Math.random() * GRID_SIZE) + 1,
-        y: Math.floor(Math.random() * GRID_SIZE) + 1,
+        x: Math.floor(Math.random() * (GRID_SIZE - 2)) + 2,
+        y: Math.floor(Math.random() * (GRID_SIZE - 2)) + 2,
     };
 }
 
